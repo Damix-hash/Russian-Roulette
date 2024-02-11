b92586d39633319ecdd624881e39c2fedee34902
Damix-hash <158813145+Damix-hash@users.noreply.github.com>
2024-02-11 21:44:19 +0100

    Update Russian-Roulette.py

--- Russian-Roulette.py
+++ Russian-Roulette.py
@@ -4,7 +4,8 @@ import random
 import time
 
 def wait(i):
-    time.sleep(int(i))
+    i = int(i)
+    time.sleep(i)
     
 dead = random.randint(1, 6)
 
@@ -12,24 +13,24 @@ print("Do you want to play russian roulette?: Y/N")
 anwser = str(input("> "))
 
 if anwser.lower() == "y":
-    print("Rules are simple.")
-    wait(1)
+    print("The rules are simple.")
+    wait(2)
     print("Press ENTER To Shoot")
-    wait(1)
+    wait(2)
     print("There's one 'bullet'")
-    wait(1)
+    wait(2)
     print("And there are 5 blanks")
-    wait(1)
+    wait(2)
     print("You gonna die if in chamber there's the bullet")
-    wait(1)
+    wait(2)
     print("How long do you want to spin? (1-10)")
     amount = int(input("> "))
     print("Spinning.")
     if amount < 11:
-        for spin in range(0, amount):
+        for i in range(0, amount):
             chamber = random.randint(1, 6)
         print("Do You Want To Shoot?")
-        wait(1)
+        wait(2)
         print("Y - Shoot")
         print("N - Skip")
         shoot_anwser = str(input("> "))
